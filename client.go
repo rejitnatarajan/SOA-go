b57e085a24d2dccd86e1cd81e82e3865b8ad53a5
Rejit Natarajan <rejit.natarajan@gmail.com>
2015-07-09 10:12:43 +0530

    Initial commit

---
+++ client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"crypto/tls"
+	"rpc"
+	"rpc/jsonrpc"
+	"log"
+)
+
+
+//jsonRequestToServer
+//jsonRequestToProvider
+
+
+//TLS Handshake
+
+
+//client fires json message to server
+
+//compose json message
+
+// Wait for response
+
+// parseResponse
+
+
+// make jsonRequestToProvider
+
+//obtain Result from Provider and print it
+
+
+
+
---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import(
+	"fmt"
+	"net"
+	"time"
+	"log"
+	"rpc"
+	"rpc/jsonrpc"
+	
+)
+
+
+
+//listen on port 9999
+
+//TLS handshake
+
+
+//obtain requestFromClient
+
+
+//find requestedMethod in GreenPagesDirectory
+
+
+//
+
+
+// Provider registers methods to GreenPagesDirectory
+
+
+//
