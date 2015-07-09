2f56dd5d7e7f82587e8abaae43621e2a9417a180
Rejit Natarajan <rejit.natarajan@gmail.com>
2015-07-09 14:28:22 +0530

    client/server tls handshake

--- client.go
+++ client.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"crypto/tls"
-	"net"
-	"rpc"
-	"rpc/jsonrpc"
+	"crypto/x509"
+	//"net"
+	//"net/rpc"
+	//"net/rpc/jsonrpc"
+	"io"
 	"log"
-	"encoding/json"
+	//"encoding/json"
 )
 
 
@@ -17,7 +18,41 @@ import (
 
 
 //TLS Handshake
+func main(){
+	
+	cert, err := tls.LoadX509KeyPair("certs/client.pem", "certs/client.key")
+	if err != nil {
+		log.Fatalf("server: loadkeys: %s", err)
+	}
+	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
+	conn, err := tls.Dial("tcp", "127.0.0.1:9999", &config)
+	if err != nil {
+		log.Fatalf("client: dial: %s", err)
+	}
+	defer conn.Close()
+	log.Println("client: connected to: ", conn.RemoteAddr())
 
+	state := conn.ConnectionState()
+	for _, v := range state.PeerCertificates {
+		fmt.Println(x509.MarshalPKIXPublicKey(v.PublicKey))
+		fmt.Println(v.Subject)
+	}
+	log.Println("client: handshake: ", state.HandshakeComplete)
+	log.Println("client: mutual: ", state.NegotiatedProtocolIsMutual)
+
+	message := "Hello\n"
+	n, err := io.WriteString(conn, message)
+	if err != nil {
+		log.Fatalf("client: write: %s", err)
+	}
+	log.Printf("client: wrote %q (%d bytes)", message, n)
+
+	reply := make([]byte, 256)
+	n, err = conn.Read(reply)
+	log.Printf("client: read %q (%d bytes)", string(reply[:n]), n)
+	log.Print("client: exiting")
+}
+	
 
 //client fires json message to server
 
--- server.go
+++ server.go
@@ -1,17 +1,15 @@
 package main
 
 import(
-	"fmt"
 	"net"
-	"time"
 	"log"
-	"io"
+	//"net/rpc"
+	//"net/rpc/jsonrpc"
+	//"io"
+	"crypto/rand"
 	"crypto/tls"
 	"crypto/x509"
-	
 )
-
-
 
 //listen on port 9999
 func main() {
@@ -29,11 +27,56 @@ func main() {
 	log.Print("server: listening")
 //TLS handshake
 
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("server: accept: %s", err)
+			break
+		}
+		log.Printf("server: accepted from %s", conn.RemoteAddr())
+		tlscon, ok := conn.(*tls.Conn)
+		if ok {
+			log.Print("ok=true")
+			state := tlscon.ConnectionState()
+			for _, v := range state.PeerCertificates {
+				log.Print(x509.MarshalPKIXPublicKey(v.PublicKey))
+			}
+		}
+		go handleClient(conn)
+	}
+}
 
-//obtain requestFromClient
 
+ //obtain requestFromClient
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 512)
+	for {
+		log.Print("server: conn: waiting")
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err != nil {
+				log.Printf("server: conn: read: %s", err)
+			}
+			break
+		}
+		log.Printf("server: conn: echo %q\n", string(buf[:n]))
+		n, err = conn.Write(buf[:n])
+
+		n, err = conn.Write(buf[:n])
+		log.Printf("server: conn: wrote %d bytes", n)
+
+		if err != nil {
+			log.Printf("server: write: %s", err)
+			break
+		}
+	}
+	log.Println("server: conn: closed")
+}
 
 //find requestedMethod in GreenPagesDirectory
+
 // lookups in id / requestedMethod Map
 // provides host/port of provider from unique id
 
